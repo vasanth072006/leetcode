8134737977275cb717d380d26134d1b2446b310d
Vasanth S <vasanth072006@gmail.com>
2025-07-23 00:03:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if (a == b && b == c) {
+            return "equilateral";
+        } else if (a == b || b == c || a == c) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
