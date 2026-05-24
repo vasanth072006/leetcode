c48dd7e5c5c8554e33cdf72e7d40cbb59f944250
Vasanth S <vasanth072006@gmail.com>
2026-05-24 19:03:09 +0530

    Time: 49 ms (17.93%), Space: 38 MB (97.32%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return True
+
+        slow = fast = head
+        reversed_list = None
+
+        # reverse left half of the list while searching
+        # the start point of the right half
+        while fast is not None and fast.next is not None:
+            tmp = slow
+
+            # keep moving guys
+            slow = slow.next
+            fast = fast.next.next
+
+            # place node at the start of the reversed half
+            tmp.next = reversed_list
+            reversed_list = tmp
+
+        # if there are even number of elements in the list
+        # do one more step to reach the first element of
+        # the second list's half
+        if fast is not None:
+            slow = slow.next
+
+        # compare reversed left half with the original
+        # right half
+        while reversed_list is not None and reversed_list.val == slow.val:
+            reversed_list = reversed_list.next
+            slow = slow.next
+
+        return reversed_list is None
